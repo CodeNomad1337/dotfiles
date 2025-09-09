d5fd7391cf89e92f21c2f2fb88a979f9805ed390
donny <>
2025-09-09 21:23:06 +0200

    Fix WezTerm install

--- scripts/run_once_install_wezterm.sh
+++ scripts/run_once_install_wezterm.sh
@@ -2,6 +2,9 @@
 set -e
 
 if command -v apt &>/dev/null; then
+    curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+    sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
     sudo apt-get install --yes wezterm
 elif command -v pacman &>/dev/null; then
     sudo pacman -Syu --noconfirm wezterm
