102d311531a9bd63f6741af6008293f312700f9b
donny <>
2025-09-12 23:33:22 +0200

    Add powerlevel10k

--- scripts/run_once_install_zsh.sh
+++ scripts/run_once_install_zsh.sh
@@ -2,11 +2,18 @@
 set -e
 
 if command -v apt &>/dev/null; then
-  sudo apt-get install --yes zsh
+    sudo apt-get install --yes zsh
 elif command -v pacman &>/dev/null; then
-  sudo pacman -Syu --noconfirm zsh
+    sudo pacman -S --noconfirm zsh
 fi
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-chsh -s $(which zsh)
+if [[ ! -d ~/.oh-my-zsh/ ]]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    chsh -s $(which zsh)
+fi
+
+if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k/ ]]
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+    sed -i 's#^ZSH_THEME=.*#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc
+fi
 
