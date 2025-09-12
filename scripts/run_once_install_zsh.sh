c64185229b156679f5ece03d3520ca273e53356b
donny <>
2025-09-12 23:39:28 +0200

    Fix if statement

--- scripts/run_once_install_zsh.sh
+++ scripts/run_once_install_zsh.sh
@@ -12,7 +12,7 @@ if [[ ! -d ~/.oh-my-zsh/ ]]; then
     chsh -s $(which zsh)
 fi
 
-if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k/ ]]
+if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k/ ]]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
     sed -i 's#^ZSH_THEME=.*#ZSH_THEME="powerlevel10k/powerlevel10k"#' ~/.zshrc
 fi
