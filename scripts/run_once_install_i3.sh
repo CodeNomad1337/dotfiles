5d8d3d8a9ce0f7bf4295b5a939490b43904a983b
donny <>
2025-09-13 12:23:06 +0200

    Add i3

---
+++ scripts/run_once_install_i3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if command -v apt &>/dev/null; then
+    sudo apt install --yes i3-wm i3blocks i3lock i3status
+elif command -v pacman &>/dev/null; then
+    sudo pacman -S --noconfirm i3-wm i3blocks i3lock i3status
+fi
+
