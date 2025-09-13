9ecab98f018afa0ffd385e0cb59f718e7a0b6718
donny <>
2025-09-13 12:29:07 +0200

    Add feh

--- scripts/run_once_install_i3.sh
+++ scripts/run_once_install_i3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if command -v apt &>/dev/null; then
-    sudo apt install --yes i3-wm i3blocks i3lock i3status
+    sudo apt install --yes i3-wm i3blocks i3lock i3status feh
 elif command -v pacman &>/dev/null; then
-    sudo pacman -S --noconfirm i3-wm i3blocks i3lock i3status
+    sudo pacman -S --noconfirm i3-wm i3blocks i3lock i3status feh
 fi
 
